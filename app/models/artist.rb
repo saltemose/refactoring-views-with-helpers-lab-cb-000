597cd1aa4d42bf684ac5c43add4cafe3c3892783
Sean Altemose <sean@seanaltemose.com>
2019-04-18 14:37:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,11 +2,12 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def artist_name
-    self.
+    @artist_name
   end
 
   def artist_name=(name)
     @artist_name = name
+    @artist_name
   end
   
 end
