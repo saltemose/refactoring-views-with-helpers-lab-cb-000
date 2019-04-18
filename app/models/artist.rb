7f74ed2696984c7916091b6cd7561ee514f5ccda
Sean Altemose <sean@seanaltemose.com>
2019-04-18 14:40:55 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,3 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-
-  def artist_name
-    @artist_name
-  end
-
-  def artist_name=(name)
-    @artist_name = name
-    @artist_name
-  end
-
 end
